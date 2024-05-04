6fbc8a101798819847240fbfed0d134de17f0d3f
Михаил Недиков <nedikov.misha@yandex.ru>
2024-05-04 14:08:13 +0300

    [update]: some problems with message with photo or not

--- src/bot/scenes/changePhoto.js
+++ src/bot/scenes/changePhoto.js
@@ -1,64 +1,172 @@
-import { Composer, Scenes } from "telegraf";
-import { updateImageUrlTokenById } from "../db/db.token/request.js";
+import { Composer, Scenes, Markup } from "telegraf";
+import {
+  GetTokenById,
+  updateImageUrlTokenById,
+} from "../db/db.token/request.js";
 import getTokenCreatedText from "../utils/getTokenCreatedText.js";
+import changeTokenInfoMarkup from "./../markups/changeTokenInfoMarkup.js";
 
 const sendMessage = new Composer();
 sendMessage.action(/changePhoto/, async (ctx) => {
-  let msg = await ctx.replyWithHTML(
-    `Submit the photo/gif you want to post`
-  );
-  ctx.wizard.state.messageToDelete = [];
-  ctx.wizard.state.messageToDelete.push(msg.message_id);
-  await ctx.deleteMessage();
-  return ctx.wizard.next();
+  try {
+    ctx.wizard.state.text = await GetTokenById(ctx.wizard.state.id);
+    ctx.wizard.state.message_id = ctx.callbackQuery.message.message_id;
+    ctx.wizard.state.chatId = ctx.callbackQuery.message.chat.id;
+    let msg;
+    if (
+      ctx.callbackQuery.message?.photo ||
+      ctx.callbackQuery.message?.video ||
+      ctx.callbackQuery.message?.animation
+    ) {
+      msg = await ctx.editMessageCaption(
+        `${getTokenCreatedText(
+          ctx.wizard.state.text
+        )}\n\nSend the <b>photo</b> you want the token to have`,
+        {
+          link_preview_options: { is_disabled: true },
+          ...Markup.inlineKeyboard([
+            Markup.button.callback(`Cancel ❌`, `cancel`),
+          ]).resize(),
+          parse_mode: `HTML`,
+        }
+      );
+    } else {
+      msg = await ctx.editMessageText(
+        `${getTokenCreatedText(
+          ctx.wizard.state.text
+        )}\n\nSend the <b>photo</b> you want the token to have`,
+        {
+          link_preview_options: { is_disabled: true },
+          ...Markup.inlineKeyboard([
+            Markup.button.callback(`Cancel ❌`, `cancel`),
+          ]).resize(),
+          parse_mode: `HTML`,
+        }
+      );
+    }
+
+    ctx.wizard.state.messageToDelete = [];
+    ctx.wizard.state.messageToDelete.push(msg.message_id);
+    return ctx.wizard.next();
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-const changePhoto = new Composer();
-changePhoto.on("photo", async (ctx) => {
-  const res = await updateImageUrlTokenById(
-    String(ctx.message.photo[ctx.message.photo.length - 1].file_id) + " photo",
-    Number(ctx.wizard.state.id)
-  );
+const change = new Composer();
+change.on("photo", async (ctx) => {
+  try {
+    const token = await updateImageUrlTokenById(
+      String(ctx.message.photo[ctx.message.photo.length - 1].file_id) +
+        " photo",
+      Number(ctx.wizard.state.id)
+    );
 
-  await ctx.replyWithPhoto(res.image_url.split(" ")[0], {
-    caption: getTokenCreatedText(res),
-    parse_mode: "HTML",
-    link_preview_options: { is_disabled: true },
-  });
-  await ctx.deleteMessage();
-  return ctx.scene.leave();
+    const media = token.image_url.split(" ")[0];
+
+    await ctx.replyWithPhoto(media, {
+      caption: getTokenCreatedText(token),
+      link_preview: false,
+      ...changeTokenInfoMarkup(ctx.wizard.state.id),
+      parse_mode: "HTML",
+    });
+    for (let i in ctx.wizard.state.messageToDelete) {
+      await ctx.deleteMessage(ctx.wizard.state.messageToDelete[i]);
+    }
+    await ctx.deleteMessage();
+
+    return ctx.scene.leave();
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-changePhoto.on("animation", async (ctx) => {
-  const res = await updateImageUrlTokenById(
-    String(ctx.message.animation.file_id) + " gif",
-    Number(ctx.wizard.state.id)
-  );
-  await ctx.replyWithDocument(res.image_url.split(" ")[0], {
-    caption: getTokenCreatedText(res),
-    parse_mode: "HTML",
-    link_preview_options: { is_disabled: true },
-  });
-  await ctx.deleteMessage();
-  return ctx.scene.leave();
+change.on("animation", async (ctx) => {
+  try {
+    const token = await updateImageUrlTokenById(
+      String(ctx.message.animation.file_id) + " gif",
+      Number(ctx.wizard.state.id)
+    );
+    await ctx.replyWithDocument(token.image_url.split(" ")[0], {
+      caption: getTokenCreatedText(token),
+      parse_mode: "HTML",
+      ...changeTokenInfoMarkup(ctx.wizard.state.id),
+      link_preview_options: { is_disabled: true },
+    });
+    for (let i in ctx.wizard.state.messageToDelete) {
+      await ctx.deleteMessage(ctx.wizard.state.messageToDelete[i]);
+    }
+    await ctx.deleteMessage();
+
+    return ctx.scene.leave();
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-changePhoto.on("video", async (ctx) => {
-  const res = await updateImageUrlTokenById(
-    String(ctx.message.video.file_id) + " video",
-    Number(ctx.wizard.state.id)
-  );
-  await ctx.replyWithVideo(res.image_url.split(" ")[0], {
-    caption: getTokenCreatedText(res),
-    parse_mode: "HTML",
-    link_preview_options: { is_disabled: true },
-  });
-  await ctx.deleteMessage();
-  return ctx.scene.leave();
+change.on("video", async (ctx) => {
+  try {
+    const res = await updateImageUrlTokenById(
+      String(ctx.message.video.file_id) + " video",
+      Number(ctx.wizard.state.id)
+    );
+    await ctx.replyWithVideo(res.image_url.split(" ")[0], {
+      caption: getTokenCreatedText(res),
+      parse_mode: "HTML",
+      ...changeTokenInfoMarkup(ctx.wizard.state.id),
+      link_preview_options: { is_disabled: true },
+    });
+    for (let i in ctx.wizard.state.messageToDelete) {
+      await ctx.deleteMessage(ctx.wizard.state.messageToDelete[i]);
+    }
+    await ctx.deleteMessage();
+
+    return ctx.scene.leave();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Отмена
+change.action("cancel", async (ctx) => {
+  try {
+    if (
+      ctx.callbackQuery.message?.photo ||
+      ctx.callbackQuery.message?.video ||
+      ctx.callbackQuery.message?.animation
+    ) {
+      await ctx.telegram.editMessageCaption(
+        ctx.wizard.state.chatId,
+        ctx.wizard.state.message_id,
+        null,
+        getTokenCreatedText(ctx.wizard.state.text),
+        {
+          link_preview_options: { is_disabled: true },
+          ...changeTokenInfoMarkup(ctx.wizard.state.id),
+          parse_mode: `HTML`,
+        }
+      );
+    } else {
+      await ctx.telegram.editMessageText(
+        ctx.wizard.state.chatId,
+        ctx.wizard.state.message_id,
+        null,
+        getTokenCreatedText(ctx.wizard.state.text),
+        {
+          link_preview_options: { is_disabled: true },
+          ...changeTokenInfoMarkup(ctx.wizard.state.id),
+          parse_mode: `HTML`,
+        }
+      );
+    }
+    return ctx.scene.leave();
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 export const ChangePhoto = new Scenes.WizardScene(
   "ChangePhoto", // first argument is Scene_ID, same as for BaseScene
   sendMessage,
-  changePhoto
+  change
 );
