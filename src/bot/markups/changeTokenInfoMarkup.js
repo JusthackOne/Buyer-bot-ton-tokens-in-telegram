ee0a388fef1b44ce53175d391e399ad8a9d2c3d7
Михаил Недиков <nedikov.misha@yandex.ru>
2024-05-05 16:28:42 +0300

    [update]: append change telegram

--- src/bot/markups/changeTokenInfoMarkup.js
+++ src/bot/markups/changeTokenInfoMarkup.js
@@ -1,10 +1,14 @@
 import { Markup } from "telegraf";
 
 export default function changeTokenInfoMarkup(id) {
-  return Markup.inlineKeyboard([
-    Markup.button.callback(`Change photo 🏞`, `changePhoto ${id}`),
-    Markup.button.callback(`Change description 📝`, `changeDescription ${id}`),
-    Markup.button.callback(`deleteToken 🟥`, `deleteToken ${id}`),
-    Markup.button.callback(`Close ❌`, `closeToken`),
-  ], { columns: 2 }).resize();
+  return Markup.inlineKeyboard(
+    [
+      Markup.button.callback(`Change photo 🏞`, `changePhoto ${id}`),
+      Markup.button.callback(`Change emoji 😀`, `changeDescription ${id}`),
+      Markup.button.callback(`Change telegram 📝`, `changeTelegram ${id}`),
+      Markup.button.callback(`deleteToken 🟥`, `deleteToken ${id}`),
+      Markup.button.callback(`Close ❌`, `closeToken`),
+    ],
+    { columns: 2 }
+  ).resize();
 }
